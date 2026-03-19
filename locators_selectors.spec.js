c4f328374d7cd1872a14509683fdb6a4d781ae25
Atikah <din.nuratikah@gmail.com>
2026-03-19 10:09:32 +0800

    add codegen selection

--- locators_selectors.spec.js
+++ locators_selectors.spec.js
@@ -47,3 +47,12 @@ test('locator for password', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
 
 });
+
+test('selector by codegen', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="error"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+});
+
